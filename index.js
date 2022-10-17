fb0a3d8d361ecc3227632d5c9a67dce9bc6c5111
hyeonukim91 <101351453+hyeonukim91@users.noreply.github.com>
2022-10-17 21:37:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+const csvUrl = [
+  'https://gist.githubusercontent.com/',
+  'curran/', // User
+  'a08a1080b88344b0c8a7/', // Id of the Gist
+  'raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/', // commit
+  'iris.csv', // File name
+].join('');
+
+
+////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
+
+const parseRow = (d) => {
+  d.sepal_length = +d.sepal_length;
+  d.sepal_width = +d.sepal_width;
+  d.petal_length = +d.petal_length;
+  d.petal_width = +d.petal_width;
+  return d;
+};
+
+const width = 800;
+const height = 600 ;
+
+
+
+// const color = (d) => {
+//   if (d.species == 'setosa')
+//     return '#A7D2CB'
+//   else if (d.species == 'versicolor')
+//     return '#F2D388'
+//   else return '#874C62'
+// }
+
+const svg = d3.select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .attr('color', '#06283D');
+
+const main = async () => {
+  const plot = scatterPlot()
+    .width(width)
+    .height(height)
+    .data(await d3.csv(csvUrl, parseRow))
+    .xValue((d) => d.petal_width)
+    .yValue((d) => d.sepal_length)
+    .margin({
+      top: 20,
+      right: 20,
+      bottom: 40,
+      left: 50,
+    })
+
+
+  svg.call(plot);
+
+  const columns = [
+    'petal_width',
+    'sepal_width',
+    'petal_length',
+    'sepal_length',
+  ];
+
+  let i = 0;
+  setInterval(() => {
+    i++;
+    plot.xValue((d) => d[columns[i % columns.length]]);
+    svg.call(plot);
+  }, 3000);
+
+};
+
+
+main();
---
+++ scatterPlot_03.js
@@ -0,0 +1,138 @@
+ const scatterPlot = () => {
+  let width;
+  let height;
+  let data;
+  let xValue;
+  let yValue;
+  let margin;
+
+
+  const my = (selection) => {
+
+    const x = d3.scaleLinear()
+      .domain([0,8])
+      .range([margin.left, width - margin.right]);
+
+    const y = d3.scaleLinear()
+      .domain(d3.extent(data, yValue))
+      .range([height - margin.bottom, margin.top]);
+
+    const icon = (d) => {
+        if (d.species == 'setosa')
+          return 'meat.svg'
+        else if (d.species == 'versicolor')
+          return 'fish.svg'
+        else return 'avocado.svg'
+      };
+
+    const marks = data.map((d) => ({
+      x: x(xValue(d)),
+      y: y(yValue(d)),
+      icon: icon(d)
+    }));
+
+    const t = d3.transition().duration(1000);
+
+    const positionG = (g) =>{
+            g.attr('transform', (d) => `translate(${d.x},${d.y})`)
+    }
+
+    const iconSvg = (svg) => {
+      svg.attr("xlink:href", (d) => d.icon);
+    }
+
+    // const positionCircles = (circles) => {
+    //   circles.attr('cx', (d) => d.x).attr('cy', (d) => d.y);
+    // };
+
+    // const initializeRadius = (circles) => {
+    //   circles.attr('r', 0);
+    // };
+    // const growRadius = (enter) => {
+    //   enter.transition(t).attr('r', radius);
+    // };
+    //
+    // const colorMark = (circles) => {
+    //   circles.attr('fill', (d) => d.color);
+    // };
+
+
+    const markg = selection
+      .selectAll('g')
+      .data(marks)
+      .attr('id', 'svgmarks')
+      .join(
+        (enter) =>
+        enter.append('g')
+        .call(positionG)
+        .append('svg:image')
+        // .attr('x', -10)
+        .attr('y', -20)
+        .attr('width', 20)
+        .attr('height', 20)
+        // .attr('transform', (d) => `rotate(30)`)
+        .call(iconSvg),
+        (update) =>
+        update.call((update) =>
+        update.transition(t)
+        .delay((d,i) => i*10)
+        .call(positionG)
+      ),//update
+          (exit) => exit.remove()
+      )//join
+
+
+
+
+
+
+    selection
+      .selectAll('.y-axis')
+      .data([null])
+      .join('g')
+      .attr('class', 'y-axis')
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y));
+
+    selection
+      .selectAll('.x-axis')
+      .data([null])
+      .join('g')
+      .attr('class', 'x-axis')
+      .attr(
+        'transform',
+        `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x));
+
+  };
+
+  my.width = function (_) {
+    return arguments.length ? ((width = +_), my) : width;
+  };
+
+  my.height = function (_) {
+    return arguments.length ? ((height = +_), my) : height;
+  };
+
+  my.data = function (_) {
+    return arguments.length ? ((data = _), my) : data;
+  };
+
+  my.xValue = function (_) {
+    return arguments.length ? ((xValue = _), my) : xValue;
+  };
+
+  my.yValue = function (_) {
+    return arguments.length ? ((yValue = _), my) : yValue;
+  };
+
+  my.margin = function (_) {
+    return arguments.length ? ((margin = _), my) : margin;
+  };
+
+  my.radius = function (_) {
+    return arguments.length ? ((radius = +_), my) : radius;
+  };
+
+  return my;
+};
